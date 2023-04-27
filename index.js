74651568a0ac6ddaca1d1202f94f6ad5e79a70b8
0mar77 <luisomar7411@gmail.com>
2023-04-27 10:03:41 -0600

    Added all packages and questions needed

--- index.js
+++ index.js
@@ -1,7 +1,89 @@
 // TODO: Include packages needed for this application
-
+const inquirer = request('inquirer');
+const fs = request('fs');
+const { generateMarkdown } = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the title of your project?',
+        validate: (answer) => {
+            if(answer === ''){
+                return 'Please enter your project title!'
+            }
+            return true
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'What was your motivation for building this project?'
+    },
+
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Why did you build this project?'
+    },
+
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'What problem does it solve?'
+    },
+
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'What did you learn creating this project'
+    },
+
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'What are the steps to install your project?'
+    },
+
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'What are the instructions for using your project? Please add exapmles.'
+    },
+
+    {
+        type: 'input',
+        name: 'Contributing',
+        message: 'How can other developers contribute to the project?'
+    },
+
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'What are some tests you have created for your project?'
+    },
+
+    {
+        type: 'list',
+        name: 'License',
+        message: 'What license do you want to use?',
+        choices:['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Common Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.0', 'Mozilla Public License', 'The Unlicense'],
+        default: 'None'
+    },
+
+    {
+        type: 'input',
+        name: 'Github Username',
+        message: 'What is your Github username?'
+    },
+
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'What is your Email?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
